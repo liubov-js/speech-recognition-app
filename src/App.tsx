3f4d42670df9b2a3ebb302e41ea728d60246aa2b
Liubov Krivtsova <avostvirk@gmail.com>
2023-07-26 15:48:11 +0300

    Add speech recognition

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useEffect, useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import MyButton from './components/UI/button/MyButton';
+import styles from './App.module.css';
 
-function App() {
+const App: FC = () => {
+  const [text, setText] = useState<string>('');
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable
+  } = useSpeechRecognition();
+
+  const startListening = () => SpeechRecognition.startListening({ continuous: true });
+
+  useEffect(() => {
+    if (text || transcript) {
+      setText(
+        transcript
+          .split(' ')
+          .map((word) => word + Math.floor(Math.random() * 100))
+          .join(' ')
+      );
+    }
+  }, [transcript, text]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  if (!isMicrophoneAvailable) {
+    return <span>Microphone isn't available.</span>;
+  }
+
+  const reset = () => {
+    resetTranscript();
+    setText('');
+  }
+
+  const listen = () => {
+    reset();
+    startListening();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <p>Микрофон: {listening ? 'включен' : 'выключен'}</p>
+      <MyButton
+        onMouseDown={listen}
+        onMouseUp={SpeechRecognition.stopListening}
+      >
+        Удерживайте для записи
+      </MyButton>
+      <MyButton onClick={reset} style={{marginLeft: '10px'}}>Сброс</MyButton>
+      <p>{text}</p>
     </div>
   );
 }
